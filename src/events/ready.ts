5782f59d7a42d068fcb7450c72bae0a112a47e54
Andrew Tandy <andrewtandy12@gmail.com>
2025-11-29 20:00:43 -0800

    refactor(events): update ready event with arrow function syntax

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,4 @@
 import { Event } from '@structures/Event';
 import { logger } from '@utils/logger';
 
-export const ready = new Event('clientReady', true).run(client => {
-  logger.info(`🟢 Logged in as ${client.user.tag}`);
-});
+export const ready = new Event('clientReady', true).run(client => logger.info(`🟢 Logged in as ${client.user.tag}`));
